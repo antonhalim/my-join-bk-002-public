4c8ff4b876df75d3e413e4595c0da83272403183
axton21 <axton_n@yahoo.com>
2015-03-27 09:38:08 -0400

    done

--- lib/my_join.rb
+++ lib/my_join.rb
@@ -1,1 +1,8 @@
-# your code goes here
+class Array
+	def my_join(arg="")
+		self.each_with_index.with_object("") do |(str, i), obj|
+			if i != self.length-1 ? obj << str.to_s << arg : obj << str.to_s
+			end
+		end
+	end
+end
